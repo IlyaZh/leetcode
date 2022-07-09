e0af7c79f10676e9bfc5155264ea5a1f9c491052
Ilia Zhitenev <iliazhitenev@ya.ru>
2022-07-09 17:08:45 +0300

    Beautify 97

--- strings/97_Interleaving_String/solution.hpp
+++ strings/97_Interleaving_String/solution.hpp
@@ -21,38 +21,20 @@ using namespace std;
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        if(s1.size() + s2.size() !  = s3.size()) return false;
+        if(s1.size() + s2.size() != s3.size()) return false;
         vector<vector<bool>> grid(s1.size()+1, vector<bool>(s2.size()+1, false));
-        grid[0][0] = true;
 
-        for(size_t r = 1; r < grid.size(); r++) {
-            if(s1.at(r-1) == s3.at(r-1)) {
-                grid[r][0] = grid[r-1][0];
-            } else {
-                grid[r][0] = false;
-            }
-        }
-        for(size_t c = 1; c < grid[0].size(); c++) {
-            if(s2.at(c-1) == s3.at(c-1)) {
-                grid[0][c] = grid[0][c-1];
-            } else {
-                grid[0][c] = false;
-            }
-        }
-
-        for(int r = 1; r < grid.size(); ++r) {
-            for(int c = 1; c < grid[0].size(); ++c) {
-                const char& sym1 = s1.at(r-1);
-                const char& sym2 = s2.at(c-1);
-                const char& sym3 = s3.at(r+c-1);
-                if(sym1 == sym3 && sym2 == sym3) {
-                    grid[r][c] = grid[r-1][c] | grid[r][c-1];
-                } else if(sym1 == sym3) {
-                    grid[r][c] = grid[r-1][c];
-                } else if(sym2 == sym3) {
-                    grid[r][c] = grid[r][c-1];
+        for(size_t r = 0; r < grid.size(); r++) {
+            for(size_t c = 0; c < grid[0].size(); c++) {
+                if(r == 0 && c == 0) {
+                    grid[r][c] = true;
+                } else if(r == 0) {
+                    grid[r][c] = grid[r][c-1] && (s2.at(c-1) == s3.at(r+c-1));
+                } else if (c == 0) {
+                    grid[r][c] = grid[r-1][c] && (s1.at(r-1) == s3.at(r+c-1));
                 } else {
-                    grid[r][c] = false;
+                    grid[r][c] =    (grid[r-1][c] && (s1.at(r-1) == s3.at(r+c-1)))
+                                 || (grid[r][c-1] && (s2.at(c-1) == s3.at(r+c-1)));
                 }
             }
         }
