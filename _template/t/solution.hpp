36eb950a483fcb2abef0d890423f9e600b593642
Ilia Zhitenev <iliazhitenev@ya.ru>
2022-07-12 23:54:27 +0300

    Added function for printing containers in template

---
+++ _template/t/solution.hpp
@@ -0,0 +1,17 @@
+// LINK TO LEETCODE
+
+// X. Title
+
+// TASK DESCTIPTION 
+
+#pragma once
+
+using namespace std;
+
+class Solution {
+public:
+    int func() {
+		int retVal = 0;
+		return retVal;
+    } 
+};
