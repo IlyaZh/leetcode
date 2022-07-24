9dd1159acd0696d2083d9e8933bd19c3e6eeb424
Ilia Zhitenev <iliazhitenev@ya.ru>
2022-07-24 09:18:18 +0300

    Upgrade template

--- _template/main.cpp
+++ _template/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <iterator>
 #include <vector>
@@ -7,21 +8,23 @@
 
 using namespace std;
 
-template<typename Container>
+template <typename Container>
 void printCont(const Container& cont) {
-    cout << "[ ";
-    copy(cont.begin(), cont.end(), ostream_iterator<typename Container::value_type>(cout, " "));
-    cout << "]";
+  cout << "[ ";
+  copy(cont.begin(), cont.end(),
+       ostream_iterator<typename Container::value_type>(cout, " "));
+  cout << "]";
 }
 
 int main(int argc, char* argv[]) {
-    const int h = 1000000000;
-    const int w = 1000000000;
-    int expect = 4;
-    Solution s;
-    cout << "IN: h = " << h << ", w = " << w << ", " << endl;
-    cout << "OUT: " << s.func() << endl;
-    cout << "Expected: " << expect << endl;
-
-    return 0;
+  const int h = 1000000000;
+  const int w = 1000000000;
+  int expect = 4;
+  Solution s;
+  auto answer = s.func();
+  cout << "IN: h = " << h << ", w = " << w << ", " << endl;
+  cout << "OUT: " << answer << endl;
+  cout << "Expected: " << expect << endl;
+  assert(answer == expect);
+  return 0;
 }
