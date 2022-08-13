696f4c6f3065190c6461fa249a31fc1293ef4fab
Ilia Zhitenev <iliazhitenev@ya.ru>
2022-08-13 09:20:50 +0300

    Improove template

--- _template/solution.hpp
+++ _template/solution.hpp
@@ -2,16 +2,16 @@
 
 // X. Title
 
-// TASK DESCTIPTION 
+// TASK DESCTIPTION
 
 #pragma once
 
 using namespace std;
 
 class Solution {
-public:
-    int func() {
-		int retVal = 0;
-		return retVal;
-    } 
+ public:
+  int func() {
+    int retVal = 0;
+    return retVal;
+  }
 };
