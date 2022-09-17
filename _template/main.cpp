7009de96f15d685173213e8777671b76ec763f74
Ilia Zhitenev <iliazhitenev@ya.ru>
2022-09-18 00:01:33 +0300

    update and improve template

--- _template/main.cpp
+++ _template/main.cpp
@@ -1,30 +1,8 @@
-#include <algorithm>
-#include <cassert>
 #include <iostream>
-#include <iterator>
-#include <vector>
 
-#include "solution.hpp"
-
-void print(bool flag) { cout << (flag ? "YES" : "NO"); }
-
-template <typename Container>
-void print(const Container& cont) {
-  cout << "[ ";
-  std::copy(cont.begin(), cont.end(),
-            std::ostream_iterator<typename Container::value_type>(cout, " "));
-  cout << "]";
-}
-
-int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
-  const int h = 1000000000;
-  const int w = 1000000000;
-  int expect = 4;
-  Solution s;
-  auto answer = s.func();
-  std::cout << "IN: h = " << h << ", w = " << w << ", " << std::endl;
-  std::cout << "OUT: " << answer << std::endl;
-  std::cout << "Expected: " << expect << std::endl;
-  assert(answer == expect && "Wrong answer");
+int main() {
+  std::cout << "use \"bazel test //path/to:tests\" to start tests and veirfy "
+               "your algorithm"
+            << std::endl;
   return 0;
 }
--- _template/solution.hpp
+++ _template/solution.hpp
@@ -4,7 +4,7 @@
 
 // TASK DESCTIPTION
 
-#pragma once
+#include <iostream>
 
 class Solution {
  public:
