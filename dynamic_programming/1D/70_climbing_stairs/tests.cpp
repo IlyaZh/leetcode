eab8f7b66092aecf5ba16820c50603d8703973a7
Ilia Zhitenev <iliazhitenev@ya.ru>
2022-11-10 21:17:15 +0100

    Added 70

---
+++ dynamic_programming/1D/70_climbing_stairs/tests.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+TEST(SolutionTest, Stairs_1) {
+  Solution s;
+  const auto result = s.climbStairs(1);
+  decltype(result) answer{1};
+  EXPECT_EQ(result, answer);
+}
+
+TEST(SolutionTest, Stairs_2) {
+  Solution s;
+  const auto result = s.climbStairs(2);
+  decltype(result) answer{2};
+  EXPECT_EQ(result, answer);
+}
+
+TEST(SolutionTest, Stairs_3) {
+  Solution s;
+  const auto result = s.climbStairs(3);
+  decltype(result) answer{3};
+  EXPECT_EQ(result, answer);
+}
+
+TEST(SolutionTest, Stairs_4) {
+  Solution s;
+  const auto result = s.climbStairs(4);
+  decltype(result) answer{5};
+  EXPECT_EQ(result, answer);
+}
