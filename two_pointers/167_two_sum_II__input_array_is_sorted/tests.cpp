81a88dd1930660e56e44e766408be64213f3eaf6
Ilia Zhitenev <iliazhitenev@ya.ru>
2022-11-20 13:59:25 +0100

    Added 167

--- two_pointers/167_two_sum_II__input_array_is_sorted/tests.cpp
+++ two_pointers/167_two_sum_II__input_array_is_sorted/tests.cpp
@@ -4,7 +4,27 @@
 
 TEST(SolutionTest, Case1) {
   Solution s;
-  const auto result = s.func();
-  decltype(result) answer{};
+  vector<int> vec = {2, 7, 11, 15};
+  int target = 9;
+  const auto result = s.twoSum(vec, target);
+  decltype(result) answer{1, 2};
+  EXPECT_EQ(result, answer);
+}
+
+TEST(SolutionTest, Case2) {
+  Solution s;
+  vector<int> vec = {2, 3, 4};
+  int target = 6;
+  const auto result = s.twoSum(vec, target);
+  decltype(result) answer{1, 3};
+  EXPECT_EQ(result, answer);
+}
+
+TEST(SolutionTest, Case3) {
+  Solution s;
+  vector<int> vec = {-1, 0};
+  int target = -1;
+  const auto result = s.twoSum(vec, target);
+  decltype(result) answer{1, 2};
   EXPECT_EQ(result, answer);
 }
