8812226fd0977af08310e6fc1d67aaa866e36811
Ilia Zhitenev <iliazhitenev@ya.ru>
2024-04-21 15:20:31 +0000

    Added 133

--- graphs/133_clone_graph/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "use \"bazel test //path/to:tests\" to start tests and veirfy "
-               "your algorithm"
-            << std::endl;
-  return 0;
-}
